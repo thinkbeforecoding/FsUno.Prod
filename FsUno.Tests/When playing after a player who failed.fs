53a68c0a4994fc9aae3e4fc328c2699737da4938
Jérémie Chassaing <jeremie.chassaing@thinkbeforecoding.com>
2014-08-09 23:14:07 +0200

    Add more tests after player failure

---
+++ FsUno.Tests/When playing after a player who failed.fs
@@ -0,0 +1,30 @@
+﻿module ``When playing after a player who failed``
+
+open Xunit
+open FsUnit.Xunit
+open Specifications
+open Deck
+open Game
+
+let gameId = GameId 1
+
+[<Fact>]
+let ``After wrong color it should still be player's turn``() =
+    Given [ GameStarted { GameId = gameId; PlayerCount = 4; FirstCard = red 3 }
+            CardPlayed { GameId = gameId; Player = 0; Card = red 9; NextPlayer = 1}
+            PlayerPlayedWrongCard { GameId = gameId; Player = 1; Card = green 7 } ]
+    |> When (PlayCard { GameId = gameId; Player = 1; Card = green 9 })
+    |> Expect [ CardPlayed { GameId = gameId; Player = 1; Card = green 9; NextPlayer = 2}]
+
+    
+
+[<Fact>]
+let ``After wrong turn it should still be expected player's turn``() =
+    Given [ GameStarted { GameId = gameId; PlayerCount = 4; FirstCard = red 3 }
+            CardPlayed { GameId = gameId; Player = 0; Card = red 9; NextPlayer = 1}
+            PlayerPlayedAtWrongTurn { GameId = gameId; Player = 3; Card = green 7 } ]
+    |> When (PlayCard { GameId = gameId; Player = 1; Card = green 9 })
+    |> Expect [ CardPlayed { GameId = gameId; Player = 1; Card = green 9; NextPlayer = 2}]
+
+    
+
