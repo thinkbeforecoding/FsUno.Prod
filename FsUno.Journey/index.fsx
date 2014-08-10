58c459debd789ab4f2e038311b8b77aaf47368b7
Jérémie Chassaing <jeremie.chassaing@thinkbeforecoding.com>
2014-08-10 14:25:11 +0200

    Journey log index links

--- FsUno.Journey/index.fsx
+++ FsUno.Journey/index.fsx
@@ -1,11 +1,15 @@
 ﻿(**
 # The journey
 
-This is the log of the journey toward building FsUno.Prod.
+This is the log of the journey toward building [FsUno.Prod](https://github.com/thinkbeforecoding/FsUno.Prod).
 
 Because the journey is more important than the destination, I'll write here 
 the things that happen along the way, things I stumbled upon, questions about design
 and implementation as well as options.
+
+The code of the project [can be found on github](https://github.com/thinkbeforecoding/FsUno.Prod)
+
+Join the discussion in the [FunctionalEventSourcing room on JabbR](https://jabbr.net/#/rooms/FunctionalEventSourcing)
 
 You can find the table of content on the right.
 *)
