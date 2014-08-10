e67cab3cf124306a0ecacc29f23c2b52469fe3ab
Jérémie Chassaing <jeremie.chassaing@thinkbeforecoding.com>
2014-08-10 16:23:07 +0200

    Fix typo

--- FsUno.Journey/Event vs Command Sourcing.fsx
+++ FsUno.Journey/Event vs Command Sourcing.fsx
@@ -1,7 +1,7 @@
 ﻿(**
 # Event Sourcing vs Command Sourcing
 
-*tldr; A lot of people read [Martin Fowler's bliki](javascript:alert('Some people could still read it and get hurt.\nThis link intentionaly left blank.'))
+*tldr; A lot of people read [Martin Fowler's bliki](javascript:alert('Some people could still read it and get hurt.\nThis link intentionally left blank.'))
 about Event Sourcing and fall in the trap.*
 
 *)
