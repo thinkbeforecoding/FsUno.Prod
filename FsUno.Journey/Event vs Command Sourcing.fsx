51ce0d07b8cc713fd2840c0bb06aeebb1726ff19
Jérémie Chassaing <jeremie.chassaing@thinkbeforecoding.com>
2014-08-10 16:17:28 +0200

    Event vs Command Sourcing

---
+++ FsUno.Journey/Event vs Command Sourcing.fsx
@@ -0,0 +1,7 @@
+﻿(**
+# Event Sourcing vs Command Sourcing
+
+*tldr; A lot of people read [Martin Fowler's bliki](javascript:alert('Some people could still read it and get hurt.\nThis link intentionaly left blank.'))
+about Event Sourcing and fall in the trap.*
+
+*)
