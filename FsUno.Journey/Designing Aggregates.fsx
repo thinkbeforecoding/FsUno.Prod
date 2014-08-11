eafe8b54a31a360e518d0e3c978469f4bbfbd801
bartelink <ruben@bartelink.com>
2014-08-11 03:31:24 +0100

    Edit of Designing Aggregates text

--- FsUno.Journey/Designing Aggregates.fsx
+++ FsUno.Journey/Designing Aggregates.fsx
@@ -3,13 +3,13 @@
 
 This evening, I start to feel that things won't move much as long as I have only one Aggregate.
 
-With several Aggregates we'll have to add Process and Proccess Managers to orchestrate the
+With several Aggregates we'll have to add Process and Process Managers to orchestrate the
 reactions to events, and ensure the global consistency of the system.
 
-We'll also have to manage projections over serveral aggregates. And make the infrastructure code
+We'll also have to manage projections over several aggregates. And make the infrastructure code
 a bit more generic to not wire it directly to the only aggregate.
 
-But before this happen: Desing !
+But before this happens: Design !
 
 ## The Game Aggregate
 
@@ -18,17 +18,16 @@ This one is already partly implemented and represents the enforcement of rules o
 ----
 
 A discussion on Jabbr with [@rbartelink](https://twitter.com/@rbartelink) interrupted me in the
-process for something awsome !
+process for something awesome !
 
 ## Algebra
 
-The first question was whether to change the ``empty`` member and make it a static member, same
-thing for the ``apply`` function.
+The first question was whether to change the ``empty`` member and make it a ``static member``, ditto the ``apply`` function.
 
 The second was whether to try to hide ``empty`` and ``apply`` completly in a ``replay`` function.
 
-``empty`` as a static member feels quite natural, but I had a problem with apply. Its signature
-seems to validate this change, but putting it before handle seems wrong.
+``empty`` as a static member feels quite natural, but I had a problem with ``apply``. Its signature
+seems to validate this change, but putting it before ``handle`` seems wrong.
 
 The problem is that when reading an Aggregate, the first important thing is the decision
 in the ``handle`` method, and changes in ``apply`` are always implemented later. Changing
@@ -68,25 +67,22 @@ Do you remember how a Monoid is defined ?
 A monoid is a triple (S,+,n) where 
 
 * S is a set
-* + is closed on the set (the signature would be S -> S -> S
+* + is closed on the set (the signature would be S -> S -> S)
 * + is associative ( x + y + z = (x + y) + z = x + (y + z) )
 * n is the neutral element ( x + n = n + x = x)
 
-Most of mathematatical structures are defined using sets (here S), functions (here +) and specific set items
-(here n).
+Most of mathematical structures are defined using sets (here `S`), functions (here `+`) and specific set items
+(here `n`).
 
 ### Aggregates
 
 Aggregates are not monoids, but you can see that it could be defined using the same
 kind of primary element:
 
-A quadruple (State, handle, apply, empty) with the rules and signature you know. ``handle`` and ``apply``
+A quadruple (State, handle, apply, empty) with the rules and signatures you know. ``handle`` and ``apply``
 are not closed on the set but the composition is not bad.
 
 If we alias ``apply`` with ``+`` we can write:
-
-    
-
 *)
     let (+) = State.apply
 
@@ -95,7 +91,7 @@ If we alias ``apply`` with ``+`` we can write:
 (**
 Makes sense, right ?
 
-The only change that I would propose is to change empty to ``initial`` because it's the **initial** state:
+The only change that I would propose is to change ``empty`` to ``initial`` because it's the **initial** state:
 *)
     type State with
         static member initial = { CurrentState = "initial state"}
@@ -105,32 +101,32 @@ The only change that I would propose is to change empty to ``initial`` because i
 (**
 Hiding ``apply`` and ``initial`` would seriously reduce the composability of all this.
 
-### High order functions
+### Higher order functions
 
 ``replay`` can actually be seen as a high order function built using ``initial`` and ``apply``.
 To build it directly from the ``State`` type we can use 
 [statically resolved type parameters](http://msdn.microsoft.com/en-us/library/dd548046.aspx),
-you can hover replay definition to see type constraints:
+you can hover over the ``replay`` definition below to see the type constraints:
 *)
     let inline replay events =
         let initial = (^S: (static member initial: ^S) ()) 
         let apply s = (^S: (static member apply: ^S -> (^E -> ^S)) s)
         List.fold apply initial events
 (**
-Ok it's a bit more complicated but you can check the signature by hovering the function, and
-it can be used on any type that has ``initial`` and apply members that have expected signature.
-And the inference still works by return type, so any hint to the compiler indicating
-that a specific Aggregate state type is in use, there is no need for further type annotation.
+Ok, it's a bit more complicated but you can check the signature by hovering the function, and
+it can be used on any type that has ``initial`` and ``apply`` members that adhere to the expected signature.
+And the inference still works from the return type, so any hint to the compiler indicating
+the specific Aggregate ``State`` type in use means there is no need for further type annotations.
 *)
     let state : State =
         [ SomethingHappened; SomethingHappened]
         |> replay
 
-(** What do we have here ?
+(** So where are we now ?
 Better vocabulary with ``initial``, better understanding about the Aggregate *algebra*, and
-a high order function function to use it.
+a high order function to use it.
 
-We can see how we'll implement more generic command handlers then.
+Now we can see how we'll implement more generic command handlers.
 
 Good evening !
 *)
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 FsUno.Prod : A Functional Event Sourcing Journey
 ================================================
 
-This is my attempt to write a more advanced version of FsUno closer from what
-would be put in production.
+This is my attempt to write a more complete version of FsUno that's
+more representative of something one would actually use in a production system.
 
 Because the journey is more important than the destination, be sure to 
 [read the full log here](http://thinkbeforecoding.github.io/FsUno.Prod/).
