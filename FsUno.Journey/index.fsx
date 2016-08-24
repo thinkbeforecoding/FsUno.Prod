35a511ca397f74512ba7eefb276fb948b190d66b
jchassaing <jeremie.chassaing@availpro.com>
2016-08-25 01:17:03 +0200

    Log:About streams lifetime

--- FsUno.Journey/index.fsx
+++ FsUno.Journey/index.fsx
@@ -15,6 +15,9 @@ You can find the table of contents on the right.
 
 ## Log entries
 
+### 2016-08-25
+* [About Streams Lifetime](About Streams Lifetime.html)
+
 ### 2016-08-24
 * [The Digit type](The Digit type.html)
 
