6919787784704b8bf9d0e8919326086ed23c11d1
Luciano Delucchi <luciano.delucchi@gmail.com>
2018-02-23 14:50:18 +0100

    Fix typo

--- FsUno.Tests/When playing a skip card.fs
+++ FsUno.Tests/When playing a skip card.fs
@@ -21,22 +21,22 @@ let ``The next player cannot play``() =
     Given [ GameStarted { GameId = gameId; PlayerCount = 4; FirstCard = red Three; FirstPlayer = 0 } 
             CardPlayed { GameId = gameId; Player = 0; Card = green Three; NextPlayer = 1}
             CardPlayed { GameId = gameId; Player = 1; Card = Skip Green; NextPlayer = 3} ]
-    |> When ( PlayCard { GameId = gameId; Player = 2; Card = green Height } )
-    |> Expect [ PlayerPlayedAtWrongTurn { GameId = gameId; Player = 2; Card = green Height } ]
+    |> When ( PlayCard { GameId = gameId; Player = 2; Card = green Eight } )
+    |> Expect [ PlayerPlayedAtWrongTurn { GameId = gameId; Player = 2; Card = green Eight } ]
 
 [<Fact>]
 let ``The second next player can play``() =
     Given [ GameStarted { GameId = gameId; PlayerCount = 4; FirstCard = red Three; FirstPlayer = 0 } 
             CardPlayed { GameId = gameId; Player = 0; Card = green Three; NextPlayer = 1}
             CardPlayed { GameId = gameId; Player = 1; Card = Skip Green; NextPlayer = 3} ]
-    |> When ( PlayCard { GameId = gameId; Player = 3; Card = green Height } )
-    |> Expect [ CardPlayed { GameId = gameId; Player = 3; Card = green Height; NextPlayer = 0 } ]
+    |> When ( PlayCard { GameId = gameId; Player = 3; Card = green Eight } )
+    |> Expect [ CardPlayed { GameId = gameId; Player = 3; Card = green Eight; NextPlayer = 0 } ]
 
 [<Fact>]
 let ``skip should skip when counterclockwise``() =
     Given [ GameStarted { GameId = gameId; PlayerCount = 4; FirstCard = red Three; FirstPlayer = 0 } 
             CardPlayed { GameId = gameId; Player = 0; Card = green Three; NextPlayer = 1}
-            CardPlayed { GameId = gameId; Player = 1; Card = green Height; NextPlayer = 2}
+            CardPlayed { GameId = gameId; Player = 1; Card = green Eight; NextPlayer = 2}
             CardPlayed { GameId = gameId; Player = 2; Card = KickBack Green; NextPlayer = 1}
             DirectionChanged { GameId = gameId; Direction = CounterClockWise }]
     |> When ( PlayCard { GameId = gameId; Player = 1; Card = Skip Green })
